dd7030460321c3cf86decf8bf1676e0df10f6d40
Ismael <Proismaelgans2017@gmail.com>
2020-04-10 13:24:43 -0400

    Ajout de component carrousel chefs gallery

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,26 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {CommonModule} from '@angular/common';
+
+import { HomeComponent } from './home/home.component';
+import { GalleryComponent } from './gallery/gallery.component';
+import { CarouselComponent } from './carousel/carousel.component';
+import { MenuComponent } from './menu/menu.component';
+import { ChefsComponent } from './chefs/chefs.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', redirectTo: '/home', pathMatch: 'full'},
+  {path: 'About', component: HomeComponent   },
+  {path: '  Menu', component: MenuComponent   },
+
+  {path: 'carousel', component:  CarouselComponent  },
+  {path: 'Chefs', component:  ChefsComponent  },
+  {path: 'Gallery', component: GalleryComponent   }
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes), CommonModule],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { GalleryComponent } from './gallery/gallery.component';
+import { CarouselComponent } from './carousel/carousel.component';
+import { MenuComponent } from './menu/menu.component';
+import { ChefsComponent } from './chefs/chefs.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    GalleryComponent,
+    CarouselComponent,
+    MenuComponent,
+    ChefsComponent
   ],
   imports: [
     BrowserModule,
